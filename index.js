eff55eec6cfe476e9de88be78462c313044b8e41
enti <enti@enti.enti>
2022-03-08 18:54:19 +0100

    Server funcionandogit add -A .

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ let archivo =	require('./images.json');
 
 let http_server = http.createServer(function(req,res) {
 	let html = "index.html";
-
-	if (req.url == "/favicon.ico"){
-		console.log("Favicon error");
-		return;
+	if (req.url == "/"){	
+		fs.readFile(html,function(err, data){
+			if (err){
+				console.log("Error2");
+				return;
+			}
+			res.writeHead(200);
+			res.end(data);
+		}); return;
 	}
 	for(let i = 0; i < archivo.images.length; i++){
 		if (req.url == "/" + archivo.images[i]){
 			fs.readFile(archivo.images[i], function(err, data) {
+				console.log(archivo.images[i]);
 				if (err) {
 					console.log("Error1");
 					return;
@@ -22,15 +28,7 @@ let http_server = http.createServer(function(req,res) {
 				res.end(data);
 			});
 		}
-	}
-	fs.readFile(html,function(err, data){
-		if (err){
-			console.log("Error2");
-			return;
-		}
-		res.writeHead(200);
-		res.end(data);
-	});
-}).listen(1095);
+	};
 
-console.log("Server running :)");
+	console.log("Server running :)");
+}).listen(1095);
